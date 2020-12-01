9417f1fc993819df3ee5fbfef1e9457c533be0c9
jzcling <jlzc89@gmail.com>
2020-12-01 13:01:00 +0800

    add certs for dev

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
+const fs = require('fs')
+
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+    "transpileDependencies": [
+      "vuetify"
+    ],
+    devServer: {
+        host: 'hubbedin.dev',
+        port: 3000,
+        https: {
+            key: fs.readFileSync("./certs/hubbedin.dev.key"),
+            cert: fs.readFileSync("./certs/hubbedin.dev.crt")
+        }
+    }
 }
